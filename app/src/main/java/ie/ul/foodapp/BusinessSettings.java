04ec7adb9e8f18ca4b40322c235418f17deaca48
ericu07 <57630135+ericu07@users.noreply.github.com>
2021-11-14 23:38:01 +0000

    Business settings layout

---
+++ app/src/main/java/ie/ul/foodapp/BusinessSettings.java
@@ -0,0 +1,16 @@
+package ie.ul.foodapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusinessSettings extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business_settings);
+    }
+}
