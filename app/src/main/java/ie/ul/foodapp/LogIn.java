f316e73deb5b98135a1a02c1348a4473d7cdbcfd
Oliver Nagy <19269749@studentmail.ul.ie>
2021-11-22 23:01:55 +0000

    Login Page  fix (with option to Sign up and Register + choice )

--- app/src/main/java/ie/ul/foodapp/LogIn.java
+++ app/src/main/java/ie/ul/foodapp/LogIn.java
@@ -1,78 +1,75 @@
 package ie.ul.foodapp;
 
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
-
 
 public class LogIn extends AppCompatActivity {
+    private EditText user_name, pass_word;
+    FirebaseAuth mAuth;
 
 
-    private static final int RC_SIGN_IN = 123;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-    }
-
-    public void OnClickSignIn (View view){
-        List<AuthUI.IdpConfig> providers  = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build()
-
-        );
-
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build() , RC_SIGN_IN);
-
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode , Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
-
-
-        if(requestCode== RC_SIGN_IN){
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-
-            if(resultCode == RESULT_OK){
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                System.out.println("Sign In Succesfull \n" +
-                        "name = " + user.getDisplayName() + "\n" +
-                        "email = " + user.getEmail() + "\n" +
-                        "id = " + user.getUid());
-                startActivity(new Intent(LogIn.this, MainActivity.class));
+        user_name=findViewById(R.id.email);
+        pass_word=findViewById(R.id.password);
+        Button btn_login = findViewById(R.id.btn_login);
+        Button btn_sign = findViewById(R.id.btn_signup);
+        mAuth=FirebaseAuth.getInstance();
+        btn_login.setOnClickListener(v -> {
+            String email= user_name.getText().toString().trim();
+            String password=pass_word.getText().toString().trim();
+            if(email.isEmpty())
+            {
+                user_name.setError("Email is empty");
+                user_name.requestFocus();
+                return;
             }
-            else {
-                if(response == null){
-                    System.out.println("Sign in cancelled");
-                    return;
-                }
-                if(response.getError().getErrorCode() == ErrorCodes.NO_NETWORK){
-                    System.out.println("No Internet Connection");
-                    return;
-                }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                user_name.setError("Enter the valid email");
+                user_name.requestFocus();
+                return;
             }
-        }
+            if(password.isEmpty())
+            {
+                pass_word.setError("Password is empty");
+                pass_word.requestFocus();
+                return;
+            }
+            if(password.length()<6)
+            {
+                pass_word.setError("Length of password is more than 6");
+                pass_word.requestFocus();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(LogIn.this, MainActivity.class));
+                }
+                else
+                {
+                    Toast.makeText(LogIn.this,
+                            "Please Check Your login Credentials",
+                            Toast.LENGTH_SHORT).show();
+                }
 
-
+            });
+        });
+        btn_sign.setOnClickListener(v -> startActivity(new Intent(LogIn.this,SignUp.class )));
     }
-
-
 
 }
